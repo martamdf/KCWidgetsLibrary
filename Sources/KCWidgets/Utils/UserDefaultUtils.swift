4ffee829de1ac1ebc23a0fa7835abc29d3a6e5ad
Marta Maquedano <marta.maquedano@gmail.com>
2024-04-23 20:38:25 +0200

    Function to update one element of an array of heroes stored in user defaults.

--- Sources/KCWidgets/Utils/UserDefaultUtils.swift
+++ Sources/KCWidgets/Utils/UserDefaultUtils.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  UserDefatultsUtils.swift
+//
 //
 //  Created by Marta Maquedano on 12/4/24.
 //
@@ -8,6 +8,11 @@
 import Foundation
 
 
+/// Function that retrieves the stored object in user defaults
+/// - Parameters:
+///   - groupName: AppGroup Name
+///   - type: Objet type (generic, although in this case the function is designed for an array of heroes)
+/// - Returns: Object
 public func getAllObjects<Object>(groupName: String, castTo type: Object.Type) -> Object? where Object: Decodable
 {
     guard let data = UserDefaults(suiteName: groupName)?.value(forKey: "heroes") as? Data  else { return nil }
@@ -21,10 +26,27 @@ public func getAllObjects<Object>(groupName: String, castTo type: Object.Type) -
     }
 }
 
-
-public func saveAllObjects<Object>(_ object: Object) where Object: Encodable {
+/// Function that save an object in user defaults
+/// - Parameters:
+///   - groupName: AppGroup Name
+///   - object: Object to save (generic, although in this case the function is designed for an array of heroes)
+public func saveAllObjects<Object>(groupName: String, _ object: Object) where Object: Encodable {
     let encoder = JSONEncoder()
     if let encoded = try? encoder.encode(object){
-        UserDefaults(suiteName: "group.DragonBallWidgetKC")?.setValue(encoded, forKey: "heroes")
+        UserDefaults(suiteName: groupName)?.setValue(encoded, forKey: "heroes")
     }
+}
+
+
+/// Function that update the object stored in user defaults  (generic, although in this case the function is designed for an array of heroes)
+/// - Parameters:
+///   - groupName: AppGroup Name
+///   - hero: element to update
+public func updateHero<Object>(groupName: String, hero: Object) where Object: Codable, Object: Equatable {
+    var heroes = getAllObjects(groupName: groupName, castTo: [Object].self)
+
+    if let index = heroes?.firstIndex(where: { $0 == hero }) {
+        heroes?[index] = hero
+    }
+    saveAllObjects(groupName: groupName, heroes)
 }
